4177349e8a05555d858431f5c32522f23b0c38af
DaffyTheDuck <34976093+DaffyTheDuck@users.noreply.github.com>
2022-09-18 22:49:50 +0530

    Added Login Facility

--- src/main/java/com/asstmngm/assetmanagement/LoginController.java
+++ src/main/java/com/asstmngm/assetmanagement/LoginController.java
@@ -1,6 +1,5 @@
 package com.asstmngm.assetmanagement;
 
-import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,12 +12,11 @@ import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.math.BigInteger;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Objects;
 
 public class LoginController {
@@ -61,32 +59,40 @@ public class LoginController {
 
 
         // use messageDigest to hash the password
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
-        byte[] digest = md.digest();
-        StringBuilder sb = new StringBuilder();
-        BigInteger bigInt = new BigInteger(1, digest);
-        passWord = bigInt.toString(16);
+//        MessageDigest md = MessageDigest.getInstance("SHA-256");
+//        byte[] digest = md.digest();
+//        StringBuilder sb = new StringBuilder();
+//        BigInteger bigInt = new BigInteger(1, digest);
+//        passWord = bigInt.toString(16);
+//        uName = userNameTextField.getText();
+
+        passWord = passwordTextField.getText();
         uName = userNameTextField.getText();
 
         DatabaseConnection connectNow = new DatabaseConnection();
         Connection connectDB = connectNow.getConnection();
 
         try {
-            String query = "SELECT * FROM user_auth WHERE userName = ? AND password = ?";
-            DatabaseConnection databaseConnection = new DatabaseConnection();
-            Connection connection = databaseConnection.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1, uName);
-            preparedStatement.setString(2, passWord);
-
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                loadOperations(event);
+            if(uName.isEmpty() || passWord.isEmpty()) {
+                failedLogin();
             } else {
+                String query = "SELECT * FROM user_auth WHERE userName = ? AND password = ?";
+                DatabaseConnection databaseConnection = new DatabaseConnection();
+                Connection connection = databaseConnection.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setString(1, uName);
+                preparedStatement.setString(2, passWord);
+
+                ResultSet resultSet = preparedStatement.executeQuery();
+
                 if(uName.isEmpty() || passWord.isEmpty()) {
                     failedLogin();
                 } else {
-                    sendToDB();
+                    while(resultSet.next()) {
+                        if(resultSet.getString("userName").equals(uName) && resultSet.getString("password").equals(passWord)) {
+                            loadOperations(event);
+                        }
+                    }
                 }
             }
         } catch (Exception e) {
@@ -99,15 +105,23 @@ public class LoginController {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Login Failed!");
         alert.setHeaderText("Login Failed!");
-        alert.setContentText("Please check your username and password!");
+        alert.setContentText("Username And Password Must Not Be Empty!");
         alert.showAndWait();
     }
 
-    private void wrongPassword() {
+    private void wrongCreds() {
         Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle("Login Failed!");
-        alert.setHeaderText("Login Failed!");
-        alert.setContentText("Please check your password!");
+        alert.setTitle("Error");
+        alert.setHeaderText("You Cannot Take This Username");
+        alert.setContentText("Username Already Exists!");
+        alert.showAndWait();
+    }
+
+    private void wrongPass() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("Invalid password");
+        alert.setContentText("The Entered Password Is Incorrect!");
         alert.showAndWait();
     }
 
